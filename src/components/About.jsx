63a6c3d237f1fdabc92c6db218f52c6561d4e58d
Owais Shariff <osh.econnect@gmail>
2024-01-17 00:36:01 +0530

    Content Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,6 +13,9 @@ const About = () => {
           Deep Pockets is not just a memecoin; it's a tribute to the audacity and grandeur of online gaming, exemplified by Drake's remarkable wagering of billions on Stake.com. 
           Operating on the Solana blockchain, Deep Pockets merges the worlds of music, gaming, and crypto into one thrilling experience, offering a token that represents more than value – it symbolizes an adventurous lifestyle.
           </p>
+          <p class="mt-5 lg:px-64 text-center text-xl text-white opacity-70">
+          We draw inspiration from the 'Drake Effect' - a term coined to describe the significant impact Drake has had on the online gaming world, particularly on Stake.com, where his username 'deeppockets6' became synonymous with high-stakes play and record-breaking wagers. Our memecoin celebrates this spirit, inviting you to be part of a community that's bold, daring, and unapologetically ambitious. Our journey is ambitious and exhilarating. Starting with our launch on Solana, we aim to develop a strong community, introduce innovative features, and establish partnerships in the entertainment and gaming industries. Our journey is ambitious and exhilarating. Starting with our launch on Solana, we aim to develop a strong community, introduce innovative features, and establish partnerships in the entertainment and gaming industries.
+          </p>
           <button
             type="button"
             class="mt-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-xl px-6 py-4 text-center me-2 mb-2"
